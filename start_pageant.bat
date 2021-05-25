d69a369c79433e8f092eb611a3472edca21d40c1
FakeEmperor <6287317+FakeEmperor@users.noreply.github.com>
2021-05-25 03:21:11 +0300

    Fixed SSH issues

--- start_pageant.bat
+++ start_pageant.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 REM Launch pageant with the ssh key I want added at startup
+
+SETX GIT_SSH D:/dev/KiTTY/plink.exe
 START pageant "L:\sec\keys\kily0@personal.ppk" "L:\sec\keys\fakeemperor@github.com.ppk" "L:\sec\keys\fakeemperor@gitlab.com.ppk"
--- start_ssh_agent.bat
+++ start_ssh_agent.bat
@@ -1,5 +1,5 @@
 @echo off
 
 REM Start wsl-ssh-pageant in a cmd instance that closes immediately (but the app keeps running).
-SETX SSH_AUTH_SOCK \\.\pipe\ssh-pageant
-START /B "" wsl-ssh-pageant-gui.exe --systray --winssh ssh-pageant --wsl %USERPROFILE%\ssh-agent.sock --force
+
+START /B "" wsl-ssh-pageant-gui.exe --systray --winssh openssh-ssh-agent --wsl %USERPROFILE%\ssh-agent.sock --force
